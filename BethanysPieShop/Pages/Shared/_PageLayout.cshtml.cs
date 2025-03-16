e2bad7b6e0b0c6eeb2e7fe1c00ea2b0ddc381076
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2025-03-16 20:21:18 +0300

    Add razor pages

---
+++ BethanysPieShop/Pages/Shared/_PageLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BethanysPieShop.Pages.Shared
+{
+    public class _PageLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
