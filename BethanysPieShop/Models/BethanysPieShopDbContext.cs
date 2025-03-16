f456449168c053bb89a237d2277e5e917c97c53f
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2025-03-16 14:59:09 +0300

    Add order feature

--- BethanysPieShop/Models/BethanysPieShopDbContext.cs
+++ BethanysPieShop/Models/BethanysPieShopDbContext.cs
@@ -9,5 +9,11 @@ namespace BethanysPieShop.Models
         public DbSet<Category> Categories { get; set; }
 
         public DbSet<Pie> Pies { get; set; }
+
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
